7dc64f36f226d3a4490a84c4f5b0f621370c4475
himanss <marplen12nm@gmail.com>
2021-09-25 21:53:35 -0400

    started the systemic template

--- scriptTemplates/systemic.js
+++ scriptTemplates/systemic.js
@@ -1,1 +1,21 @@
-print("systemic v1.0")
+
+//this asserts that both file and its parents exist
+{
+let l = assertFile("/startup/splash")
+if(l.value == null)l.exec = (...args)=>{
+  print("systemic v1.0")
+};
+}
+
+let home = assertFile("/home/"); if(home.value == null)home.value = {};
+let sys = assertFile("/sys/"); if(sys.value == null)sys.value = {};
+
+
+cd("/sys/")
+
+{
+let l = assertFile("/startup/")
+l.list.forEach((v, i) => v.exec() );
+}
+
+cd("/home/")
